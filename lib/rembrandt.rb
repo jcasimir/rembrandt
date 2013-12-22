8796ced3274545227798f12d87509deecc9acb98
Jeff Casimir <jeff@casimircreative.com>
2013-12-22 00:22:15 -0700

    Require subfiles from the primary

--- lib/rembrandt.rb
+++ lib/rembrandt.rb
@@ -1,4 +1,8 @@
-require "rembrandt/version"
+require_relative "rembrandt/version"
+require_relative "rembrandt/highlighter"
+require_relative "rembrandt/engines/pygmentize"
+require_relative "rembrandt/engines/web_service"
+require_relative "rembrandt/formatters/table"
 
 module Rembrandt
 end
