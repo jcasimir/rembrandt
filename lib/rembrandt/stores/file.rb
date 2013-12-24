34de24ffe31b8989f2c0b76b80d386cf6f027664
Jeff Casimir <jeff@casimircreative.com>
2013-12-24 14:47:09 -0700

    File store reads and writes files with a provided key

--- lib/rembrandt/stores/file.rb
+++ lib/rembrandt/stores/file.rb
@@ -6,12 +6,35 @@ module Rembrandt
       attr_reader :output_directory
 
       def initialize(directory)
-        @output_directory = directory
+        self.output_directory = directory
         create_directory
+      end
+
+      def output_directory=(name)
+        name += '/' unless name.end_with?('/')
+        @output_directory = name
       end
 
       def create_directory
         FileUtils.mkdir_p output_directory
+      end
+
+      def write(key, data)
+        ::File.open( filename_for(key), "w" ) do |f|
+          f.write(data)
+        end
+      end
+
+      def read(key)
+        ::File.read filename_for(key)
+      end
+
+      def filename_for(key)
+        output_directory + prefix + key
+      end
+
+      def prefix
+        'rembrandt_'
       end
     end
   end
--- test/rembrandt/stores/file_test.rb
+++ test/rembrandt/stores/file_test.rb
@@ -4,11 +4,27 @@ require './lib/rembrandt/stores/file'
 class StoresFileTest < CodeHighlightTest
   attr_reader :store
 
+  def target
+    './tmp/.rembrandt_cache'
+  end
+
   def test_it_creates_the_storage_folder
-    target = './tmp/.rembrandt_cache'
     `rm -rf #{target}` if Dir.exist?(target)
     refute Dir.exist?(target), "Directory #{target} should not exist."
     @store = Rembrandt::Stores::File.new(target)
     assert Dir.exist?(target), "Directory #{target} should exist."
+  end
+
+  def test_it_fetches_data_from_a_file
+    sample = "Hello, World!"
+    key = "001"
+    store = Rembrandt::Stores::File.new(target)
+    store.write(key, sample)
+    fetched = store.read(key)
+    assert_equal sample, fetched
+  end
+
+  def teardown
+    `rm -rf #{target}/*` if Dir.exist?(target)
   end
 end
